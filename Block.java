4cdda3f3196873eca8850dae55e3e1c090e2ccf4
Doğancan <dhirdavatcioglu@yahoo.com>
2021-09-01 22:32:50 +0300

    Block added

---
+++ Block.java
@@ -0,0 +1,12 @@
+public class Block {
+    public String hash;
+    public String previousHash;
+    private String data;
+    private long timeStamp;
+
+    public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = timeStamp;
+    }
+}
